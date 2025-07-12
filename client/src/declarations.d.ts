2f451f2858042648ece850881813b8db097571e7
Oleg Ilin <olegilin@MacBook-Air-Oleg.local>
2025-07-12 16:28:45 +0300

    upd command build

---
+++ client/src/declarations.d.ts
@@ -0,0 +1,12 @@
+// src/declarations.d.ts
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.css";
